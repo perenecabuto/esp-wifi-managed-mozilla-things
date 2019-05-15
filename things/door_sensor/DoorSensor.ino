fde1480f2749f1bda442b0a5dce4d2f93d55b409
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2019-05-15 20:33:29 -0300

    feat: add DoorSensor thing

---
+++ things/door_sensor/DoorSensor.ino
@@ -0,0 +1,40 @@
+#include "common/Base.h"
+
+#define SENSOR_PIN 2
+
+WebThingAdapter* adapter;
+const char* sensorTypes[] = {"DoorSensor", "Sensor", nullptr};
+ThingDevice sensor("door-sensor", "DoorSensor", sensorTypes);
+ThingProperty sensorMotion("open", "MotionProperty", BOOLEAN, "OpenProperty");
+
+void setup(void) {
+  initWifi(sensor.name);
+
+  adapter = new WebThingAdapter("door-sensor", WiFi.localIP());
+  sensorMotion.readOnly = true;
+  sensor.addProperty(&sensorMotion);
+  adapter->addDevice(&sensor);
+  adapter->begin();
+
+  setPullDownPin(SENSOR_PIN);
+}
+
+void loop(void) {
+  int doorIsOpen = digitalRead(SENSOR_PIN) == LOW;
+  Serial.println(doorIsOpen ? "door open" : "door closed");
+
+  ThingPropertyValue motion;
+  motion.boolean = doorIsOpen;
+  sensorMotion.setValue(motion);
+
+  setPullDownPin(SENSOR_PIN);
+
+  adapter->update();
+  delay(500);
+}
+
+void setPullDownPin(int pin) {
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+  pinMode(pin, INPUT);
+}
