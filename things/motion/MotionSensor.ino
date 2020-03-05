22bf1dcce70ace0b65ad07aee66eccfd47bd5870
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2020-03-05 14:54:03 -0300

    feat: remove level property from montion sensor

--- things/motion/MotionSensor.ino
+++ things/motion/MotionSensor.ino
@@ -6,16 +6,13 @@ WebThingAdapter* adapter;
 const char* sensorTypes[] = {"MotionSensor", "Sensor", nullptr};
 ThingDevice sensor("motion", "MotionDetector", sensorTypes);
 ThingProperty sensorMotion("motion detected", "MotionProperty", BOOLEAN, "MotionProperty");
-ThingProperty sensorData("motion data", "LevelProperty", NUMBER, "LevelProperty");
 
 void setup(void) {
   initWifi(sensor.id);
 
   adapter = new WebThingAdapter("motion-sensor", WiFi.localIP());
   sensorMotion.readOnly = true;
-  sensorData.readOnly = true;
   sensor.addProperty(&sensorMotion);
-  sensor.addProperty(&sensorData);
   adapter->addDevice(&sensor);
   adapter->begin();
 
@@ -23,22 +20,15 @@ void setup(void) {
 }
 
 void loop(void) {
-  int sensorValue = analogRead(SENSOR_PIN);
-  bool motionDetected = (sensorValue > 1024);
+  bool motionDetected = digitalRead(SENSOR_PIN) == HIGH;
+
+  if (motionDetected) {
+    Serial.println("Motion detected!");
+  }
 
   ThingPropertyValue motion;
   motion.boolean = motionDetected;
   sensorMotion.setValue(motion);
-
-  ThingPropertyValue val;
-  val.number = sensorValue;
-  sensorData.setValue(val);
-
-  Serial.print("sensorValue:");
-  Serial.print(sensorValue);
-  Serial.print(", detected:");
-  Serial.print(motionDetected);
-  Serial.println("");
 
   adapter->update();
   delay(500);
